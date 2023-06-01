a5e1ff8373026670aac5c6375222108301f048b1
Raymond Andreassen <Raymond.Andreassen@uit.no>
2023-06-02 01:57:02 +0200

    Remove

--- Terraform/Terraform_apply.ps1
+++
@@ -1,9 +0,0 @@
-
-
-terraform apply -var-file .\01.secrets.tfvars
-
-sleep 1
-
-$filename = "terraform_" + (Get-Date -Format "yyyy-MM-dd_HH-mm-ss") + ".tfstate"
-# Compress-Archive -Path .\..\Terraform -DestinationPath ..\..\AzureApplicationSolutionBackup\$filename
-Copy-Item -Path .\terraform.tfstate .\..\..\AzureApplicationSolutionBackup\$filename 
